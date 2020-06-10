d55b6d52dd99d0aa21dc877978afd356fb2ae20e
Anamika Gupta <anamikag594@gmail.com>
2020-06-10 16:00:34 +0530

    Css changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ class App extends Component {
    render() {
       return (
          <div>
-         <ul>
+         <ul className="menu">
           <li><Link to="/">App</Link></li>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
          </ul>
+
+        
          </div>
       )
    }
